c96b7bdf55e34879b8cc1d913e56e907ea864869
Ryan Joya <ryanjoya@users.noreply.github.com>
2015-07-31 22:29:03 -0500

    Update 15.15 median.py

--- 15.15 median.py
+++ 15.15 median.py
@@ -1,1 +1,8 @@
-
+def median(lst):
+    lst.sort()
+    if len(lst)==1:
+        return lst[0]
+    elif len(lst)%2 != 0:
+        return lst[(len(lst)+1)/2-1]
+    else:
+        return (lst[len(lst)/2-1]+lst[len(lst)/2])/2.0
